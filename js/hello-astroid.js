eaa4577008baf9810d7e644f046724c246d4a44f
Jennifer Deleon <jennifer.deleon404@gmail.com>
2021-12-08 12:04:17 -0600

    fixed folder location

---
+++ js/hello-astroid.js
@@ -0,0 +1,4 @@
+console.log("Hello, sweet asteroid!");
+var toEat = "Something I really like to eat is"
+var favoriteFood =  " Pizza.....supreme." + " Side salad with italian dressing or vingar based dressing with lots of black olives."
+    console.log( toEat + favoriteFood)
