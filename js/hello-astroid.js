0fb62f0164e119149fad52298d838a1a65cedba5
Jennifer Deleon <jennifer.deleon404@gmail.com>
2021-12-08 12:28:12 -0600

    finished day 3 assignment

--- js/hello-astroid.js
+++ js/hello-astroid.js
@@ -1,4 +1,4 @@
 console.log("Hello, sweet asteroid!");
-var toEat = "Something I really like to eat is"
-var favoriteFood =  " Pizza.....supreme." + " Side salad with italian dressing or vingar based dressing with lots of black olives."
-    console.log( toEat + favoriteFood)
+var toEat = "\nSomething I really like to eat is: \n";
+var favoriteFood =  "\tPizza..... \n\t\tsupreme." + "\n\t\t\tAlso, side salad \n\t\t\t\twith italian dressing \n\t\t\t\t\tor....any vinegar based dressing with lots \n\t\t\t\t\t\tof \n\t\t\t\t\t\t\tblack \n\t\t\t\t\t\t\t\tolives.";
+    console.log( toEat + favoriteFood);
