5e551dd843c2abb29e781a2ea78b1b973df4f10a
Jennifer Deleon <jennifer.deleon404@gmail.com>
2021-12-09 08:36:37 -0600

    primitive type assignment

---
+++ js/primitive-types.js
@@ -0,0 +1,38 @@
+// Identify the data type of the following values:
+//
+//
+//  99.9
+//  Number
+//
+// "false"
+// string
+//
+// false
+// boolean
+//
+// '0'
+// string
+//
+// 0
+// number
+//
+// true
+// boolean
+//
+// 'true'
+//string
+
+// What data type would best represent:
+//
+//     A term or phrase typed into a search box?
+// string
+//     If a user is logged in?
+// boolean
+//     A discount amount to apply to a user's shopping cart?
+// number
+// Whether or not a coupon code is valid?
+//boolean
+//     An email address typed into a registration form?
+// boolean
+//     The price of a product?
+// number
