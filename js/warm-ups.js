f65a2b3fed71bd87f063e394765beec15385e90e
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-01-04 16:23:54 -0600

    Done with array practice

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -121,3 +121,14 @@
 // }
 //
 // getPassword()
+
+// Mini Exercise
+// -Create an array with five strings representing your favorite artists (music or otherwise) and store it in a variable called favoriteArtists
+// -Write a function that accepts an array as a parameter and...
+// -> logs the first, third, and last elements of that array
+// -Invoke your function and pass favoriteArtists as an argument
+var favArtists = ['Circa', 'Midnight', 'Bishop', 'Timecop', 'Random']
+function array (x){
+    return console.log(favArtists[0], (favArtists[2]), (favArtists[4]));
+}
+array(favArtists)
