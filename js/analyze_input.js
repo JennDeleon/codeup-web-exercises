dd3004c3c3669ce73752d379d8d22db8570de8fe
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-01-04 21:10:10 -0600

    finished analyze_input

--- js/analyze_input.js
+++ js/analyze_input.js
@@ -1,33 +1,15 @@
 
 //     In that file, write a program that receives user input and analyzes the input differently if the input is a number or a string. If the input is a string, it will analyze the string differently if it is length 1.
 
-var input = prompt("Whats your number?");
-function analyze(input){
-    if (!isNaN(input)){
-        alert("Thats a number");
-    } else if (typeof input === "string")
-    {
-        alert("Thats a string");
-    }
-}
-analyze(input);
-
-
 // Following are the rules of analysis:
 //     If the input is a number:
 //     Output if the number is odd or even (zero is considered even). Use your function from Exercise 2 for this functionality.
-
-
-analyze(num)
-
 
 //     Output if the number is negative, positive, or zero
 // If the input is a string:
 
 //     If the string’s length is 1, output if the string (or char) is a vowel or not
 // Otherwise, output the length of the string
-
-
 
 // Design your program to include a switch and at least one if/else. You must also make separate functions for all of the following program functionality:
 //     Determining if the input is a number, string, or otherwise. This function should be called analyzeInput
@@ -36,6 +18,41 @@ analyze(num)
 //     Determining if the string is a single character or not
 //     Determining if the length 1 string is a vowel or not
 
+var input = prompt("Whats your number?");
+function analyze(input){
+    if (!isNaN(input)){
+        alert("Thats a number");
+        if (input % 2 === 0){
+            alert("Thats an even number");
+        } else {
+            alert("Thats an odd number.");
+        }
+        if (input > 0) {
+            alert("The number is positive");
+        }
+        else if (input == 0){
+            alert("The number is zero");
+        }
+        else {
+            alert("The number is negative");
+        }
+    } else if (typeof input === "string") {
+        alert("Thats a string");
+        switch (input.toLowerCase()) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'y':
+                alert("Thats a vowel")
+                break;
+            default:
+                alert("Thats not a vowel")
+        }
+    }
+}
+analyze(input);
 
 
 //     Add, commit, and push to GitHub.
