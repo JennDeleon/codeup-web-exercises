b90a1e73276818983cb0913ebee11e720a40059e
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-01-10 11:07:08 -0600

    extra exercises

---
+++ Extra exercises/extra-functions.js
@@ -0,0 +1,32 @@
+// Make a function called returnTwo() that returns the number 2 when called
+// Test this function with console.log(returnTwo())
+function returnTwo(num){
+    return 2;
+}
+console.log(returnTwo());
+//     Make a function called sayHowdy() which console.logs the string “Howdy!”
+function sayHowdy(str){
+    console.log("Howdy!");
+}
+// Test this function by directly calling sayHowdy()
+// Remember this function does not need a defined return value
+sayHowdy();
+// Make a function called returnName() that returns the string of your name
+function returnName(str){
+    return "Jennifer";
+}
+// Test this function with console.log(returnName())
+console.log(returnName())
+//     Make a function called addThree() which takes in a number input and returns the number plus 3.
+// Test this function with console.log(addThree(5))
+function addThree(num){
+    return (num + 3);
+}
+console.log(addThree(2)); //expected output 5
+console.log(addThree(4)); //expected output 7
+//     Make a function called sayString() which returns the string input passed in.
+// Test this function with console.log(sayString('codeup'))
+function sayString(str){
+    return str;
+}
+console.log(sayString("Return this string"));
